e41d2af09f3c2c4c3ec35dfcb570b0b9d80d1dae
yash0307 <yash.patel@students.iiit.ac.in>
2018-04-10 23:46:07 -0400

    + sync

--- Q_agent.py
+++ Q_agent.py
@@ -12,6 +12,23 @@ from sklearn import svm
 from sklearn.externals import joblib
 from sklearn.metrics import average_precision_score, accuracy_score
 from sklearn import preprocessing
+from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential, Model, load_model
+from keras.layers import *
+from keras.layers.pooling import GlobalAveragePooling1D
+from keras.callbacks import LearningRateScheduler
+from keras.callbacks import ModelCheckpoint
+import keras.backend as K
+import keras
+import numpy as np
+import gym
+import sys
+import copy
+import argparse
+from collections import deque
+import random
 
 ##### Load Data #####
 def data_loader(source_domain, target_domain, num_s_pos_samples, num_reward_samples, data_dir):
@@ -55,21 +72,58 @@ def gen_s_pos_and_reward_set(domain_1_reps, domain_2_reps, domain_1_labels, doma
 			given_sample_distance = sample_distances[given_sample]
 			class_dist_dict[given_class].append((given_sample, given_sample_distance))
 	
-	s_pos = {}
 	reward_set = {}
 	for given_class in class_dist_dict.keys():
-		if given_class not in s_pos.keys():
-			s_pos[given_class] = []
+		if given_class not in reward_set.keys():
 			reward_set[given_class] = []
 		given_class_list = sorted(class_dist_dict[given_class], key=lambda x:x[1])
 		for given_sample in given_class_list[:num_reward_samples]:
 			reward_set[given_class].append(given_sample[0])
-		for given_sample in given_class_list[len(given_class_list)-num_s_pos_samples:]:
-			s_pos[given_class].append(given_sample[0])
+
+	s_pos = []
+	sample_distances_pairs = []
+	for given_sample in range(0, len(sample_distances)):
+		sample_distances_pairs.append((given_sample, sample_distances[given_sample]))
+
+	sorted_sample_dist = sorted(sample_distances_pairs, key=lambda x:x[1])
+	for given_sample in sorted_sample_dist[len(sorted_sample_dist)-num_s_pos_samples:]:
+		s_pos.append(given_sample[0])
 	return s_pos, reward_set
 ##### End Initialization #####
 
 ##### Train n-way classifier #####
+def train_SVM(s_pos, domain_2_labels, domain_2_reps, rep_dim):
+	num_samples = len(s_pos)
+	X = np.zeros((num_samples, rep_dim), dtype='float')
+	Y = np.zeros((num_samples, 1), dtype='float')
+	for i in range(0, num_samples):
+		given_sample = s_pos[i]
+		X[i,:] = domain_2_reps[given_sample,:]
+		Y[i] = domain_2_labels[given_sample]
+	scaler = preprocessing.StandardScaler().fit(X)
+        X_scaled = scaler.transform(X)
+        given_SVM = svm.LinearSVC(multi_class='ovr')
+        given_SVM.fit(X_scaled, Y)
+	return given_SVM
+
+def check_accu(reward_set, domain_2_reps, given_SVM, rep_dim):
+	num_samples = sum([len(reward_set[i]) for i in reward_set.keys()])
+	X = np.zeros((num_samples, rep_dim), dtype='float')
+	Y = np.zeros((num_samples, 1), dtype='float')
+	idx = 0
+	for given_class in reward_set.keys():
+		list_class = reward_set[given_class]
+		for given_sample in list_class:
+			X[idx,:] = domain_2_reps[given_sample,:]
+			Y[idx] = given_class
+			idx += 1
+	scaler = preprocessing.StandardScaler().fit(X)
+	X_scaled = scaler.transform(X)
+	YY_ = given_SVM.predict(X_scaled)
+	Y = Y.flatten()
+	accu = accuracy_score(Y, YY_)
+	return accu
+
 ##### End Train n-way classifier #####
 
 ##### Unlabeled predictions #####
@@ -88,13 +142,20 @@ def get_unlabelled_predictions(domain_1_reps, domain_2_reps, domain_1_labels):
 ##### End Unlabeled predictions #####
 
 ##### Make State Representation #####
+def get_histograms(num_hist, domain_2_reps, given_SVM):
+	num_samples = domain_2_reps.shape[0]
+	given_hist = np.zeros((num_samples, num_hist), dtype='float')
+	for i in range(0, num_samples):
+	
+	return
 ##### End Make State Representation #####
 
 if __name__ == '__main__':
 	source_domain = 'amazon'
 	target_domain = 'dslr'
-	num_s_pos_samples = 6
+	num_s_pos_samples = 50
 	num_reward_samples = 3
+	max_iters = 2000
 	data_dir = '/home/yash/Sem2/DeepRL/Project/Amazon-finetune-features/'
 
 	# Load the data
@@ -105,3 +166,7 @@ if __name__ == '__main__':
 
 	domain_2_labels = get_unlabelled_predictions(domain_1_reps, domain_2_reps, domain_1_labels)
 	
+	for given_iter in range(0, max_iters):
+		given_SVM = train_SVM(s_pos, domain_2_labels, domain_2_reps, rep_dim)
+		given_accu = check_accu(reward_set, domain_2_reps, given_SVM, rep_dim)
+		print(given_accu)
